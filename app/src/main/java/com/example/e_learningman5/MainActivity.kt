366be1d80a5bf18bfb7ab132a76258fb59499fe9
raafi-4z1 <raafia452@gmail.com>
2024-03-03 14:55:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/e_learningman5/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.e_learningman5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.e_learningman5.ui.theme.ELearningMan5Theme
+
+@ExperimentalMaterial3Api
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ELearningMan5Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Isi
+                }
+            }
+        }
+    }
+}
