887379ef75d1bd8d468d25e418407d5989ae7b50
Raafi Azizan Alim <raafia452@gmail.com>
2024-03-05 15:19:30 +0700

    Update LoginScreen.kt

--- app/src/main/java/com/example/e_learningman5/login/features/LoginScreen.kt
+++ app/src/main/java/com/example/e_learningman5/login/features/LoginScreen.kt
@@ -1,4 +1,4 @@
-package com.example.e_learningman5.login.features
+package com.example.e_learningman5.login.feature
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
